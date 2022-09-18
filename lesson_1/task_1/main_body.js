ce5319d3419373a677d911b8c046182e29f20a55
Alexvmar72 <97352345+Alexvmar72@users.noreply.github.com>
2022-09-18 19:39:51 +0300

    Delete directory

--- lesson_1/task_1/main_body.js
+++
@@ -1,5 +0,0 @@
-"use strict"
-
-let temp_celci = Number(prompt("Введите температуру в градусах цельсия:"))
-let temp_faren = (9 / 5) * temp_celci + 32
-alert('Цельсий: ' + temp_celci + '  Фаренгейт: ' + temp_faren)
