7e934efc1e965d1ec2269c540c8dffec15f546d0
Alex Mark <alexvmar@yandex.ru>
2022-09-18 19:36:11 +0300

    Created files

---
+++ lesson_1/task_1/main_body.js
@@ -0,0 +1,5 @@
+"use strict"
+
+let temp_celci = Number(prompt("Введите температуру в градусах цельсия:"))
+let temp_faren = (9 / 5) * temp_celci + 32
+alert('Цельсий: ' + temp_celci + '  Фаренгейт: ' + temp_faren)
